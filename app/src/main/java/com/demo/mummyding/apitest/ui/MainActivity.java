4a28e9ffc37a846178929ef7cecbff9859a59b48
MummyDing <mummydingz@gmail.com>
2015-11-10 23:53:45 +0800

    测试下拉刷新&网页数据缓存

--- app/src/main/java/com/demo/mummyding/apitest/ui/MainActivity.java
+++ app/src/main/java/com/demo/mummyding/apitest/ui/MainActivity.java
@@ -22,6 +22,7 @@ import com.demo.mummyding.apitest.adapter.NewsAdapter;
 import com.demo.mummyding.apitest.cache.PolicyCache;
 import com.demo.mummyding.apitest.model.PolicyBean;
 import com.demo.mummyding.apitest.sax.SAXParse;
+import com.yalantis.phoenix.PullToRefreshView;
 
 import org.xml.sax.SAXException;
 
@@ -32,18 +33,18 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.xml.parsers.ParserConfigurationException;
-
 public class MainActivity extends AppCompatActivity {
 
-
     private ListView listView;
+    private PullToRefreshView refreshView;
     /*private TextView textView;
     private WebView webView;*/
     private RequestQueue queue;
-    private  List<PolicyBean> items ;
+    private  List<PolicyBean> items = new ArrayList<>();
     private NewsAdapter adapter;
     private PolicyCache cache = new PolicyCache(this);
     @Override
@@ -64,14 +65,27 @@ public class MainActivity extends AppCompatActivity {
         cache = new  PolicyCache(this);
         items = cache.loadFromCache();
         listView = (ListView) findViewById(R.id.listnews);
+         refreshView = (PullToRefreshView) findViewById(R.id.pull_to_refresh);
         adapter = new NewsAdapter(MainActivity.this,R.layout.item_news,items);
         listView.setOnItemClickListener(adapter);
-        listView.setAdapter(adapter); 
+        listView.setAdapter(adapter);
         if(items.size() == 0 ){
             loadDataFromNet();
         }else{
             Log.d("size",items.size()+"");
         }
+        refreshView.setOnRefreshListener(new PullToRefreshView.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshView.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        loadDataFromNet();
+                        refreshView.setRefreshing(false);
+                    }
+                });
+            }
+        });
     }
     private void loadDataFromNet(){
         Log.d("net","new is working");
@@ -82,9 +96,11 @@ public class MainActivity extends AppCompatActivity {
                     @TargetApi(Build.VERSION_CODES.KITKAT)
                     @Override
                     public void onResponse(String s) {
+                        Log.d("respon","res");
                         InputStream is =
                                 new ByteArrayInputStream(s.getBytes(StandardCharsets.ISO_8859_1));
                         try {
+                            items.clear();
                             items.addAll(SAXParse.parse(is));
                         } catch (ParserConfigurationException e) {
                             e.printStackTrace();
@@ -98,9 +114,12 @@ public class MainActivity extends AppCompatActivity {
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError volleyError) {
-                Toast.makeText(MainActivity.this,"error",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this,"网络异常",Toast.LENGTH_SHORT).show();
+            //    Log.d("respon", "errror");
             }
         });
+        stringRequest.setShouldCache(false);
         queue.add(stringRequest);
+        Log.d("end", "end");
     }
 }
--- app/src/main/java/com/demo/mummyding/apitest/ui/WebDetailsActivity.java
+++ app/src/main/java/com/demo/mummyding/apitest/ui/WebDetailsActivity.java
@@ -2,18 +2,51 @@ package com.demo.mummyding.apitest.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.demo.mummyding.apitest.R;
 
 public class WebDetailsActivity extends AppCompatActivity {
 
     private WebView webView;
+    private boolean flag = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_details);
+        String url  = getIntent().getStringExtra("url");
         webView = (WebView) findViewById(R.id.webview);
-        webView.loadUrl(getIntent().getStringExtra("url"));
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setDatabaseEnabled(true);
+        String path = getFilesDir().getAbsolutePath()+"/webcache";
+        webView.getSettings().setDatabasePath(path);
+        webView.getSettings().setAppCachePath(path);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.loadUrl(url);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                webView.loadUrl(url);
+                return true;
+            }
+
+
+        });
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
